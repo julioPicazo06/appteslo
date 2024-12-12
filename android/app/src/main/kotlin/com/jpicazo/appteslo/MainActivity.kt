57a1e16cddc7e49ec0c390a1c3f8dd61c130b061
jpicazo <juliopicazo06@gmail.com>
2024-12-11 18:43:39 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/jpicazo/appteslo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jpicazo.appteslo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:appteslo/config/router/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:appteslo/config/config.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      theme: AppTheme().getTheme(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
